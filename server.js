e8a9e2b2c9c7177a5cd03ef0114bc3696637eae2
joshuanikolas <nikolassnchez@icloud.com>
2024-08-28 19:23:48 -0500

    exercise 4 done

--- server.js
+++ server.js
@@ -7,6 +7,18 @@ const collectibles = [
     { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
   ];
 
+  const shoes = [
+    { name: "Birkenstocks", price: 50, type: "sandal" },
+    { name: "Air Jordans", price: 500, type: "sneaker" },
+    { name: "Air Mahomeses", price: 501, type: "sneaker" },
+    { name: "Utility Boots", price: 20, type: "boot" },
+    { name: "Velcro Sandals", price: 15, type: "sandal" },
+    { name: "Jet Boots", price: 1000, type: "boot" },
+    { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+];
+
+
+// 1. Be Polite, Greet the User
 
 app.get('/greetings/:name', (req, res) => {
         const name = req.params.name;
@@ -14,6 +26,7 @@ app.get('/greetings/:name', (req, res) => {
 });
 
 
+//2. Rolling the Dice
 
 
 app.get('/roll/:number', (req, res) => {
@@ -26,6 +39,8 @@ app.get('/roll/:number', (req, res) => {
         }
         });
 
+
+//3. I Want THAT One!
 
 
         app.get('/collectibles/:index', (req, res) => {
@@ -40,8 +55,30 @@ app.get('/roll/:number', (req, res) => {
         });
 
 
+//4. Filter Shoes by Query Parameters
 
 
+        app.get('/shoes', (req, res) => {
+            const minPrice = (req.query.min_price)
+            const maxPrice = (req.query.max_price)
+            const type = (req.query.type)
+        
+            const filteredShoes = shoes.filter(shoe => {
+                let keep = true
+                if(minPrice && shoe.price < minPrice) {
+                    keep = false
+                }
+                if(maxPrice && shoe.price > maxPrice) {
+                    keep = false
+                }
+                if(type && shoe.type !== type) {
+                    keep = false
+                }
+                return keep
+            })
+        
+            res.json(filteredShoes)
+        });
 
 
 
